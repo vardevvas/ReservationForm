7fd9a474a773729428234891833aace8e2ad88e6
vardevvas <vardevvas@gmail.com>
2023-05-21 16:28:36 +0200

    first commit

---
+++ LittleLemonReservation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LittleLemonReservation
+//
+//  Created by Ali Mohammed on 19/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LittleLemonReservation/LittleLemonReservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonReservationApp.swift
+//  LittleLemonReservation
+//
+//  Created by Ali Mohammed on 19/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonReservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
